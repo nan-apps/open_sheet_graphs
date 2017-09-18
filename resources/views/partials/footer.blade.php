8c81639c9e88713c62c8edd2575166fc4be99c7c
Gaston Nan <nangastonr@gmail.com>
2017-09-17 22:09:38 -0300

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,15 +2,21 @@
   <div class="container">
     <div class="content has-text-centered">
       <p>
-        <strong>Padrón de femicidios</strong> por <a href="http://lavaca.org">lavaca.org</a>         
+        <strong>Padrón de femicidios</strong> por <a target="_blank" href="http://lavaca.org">lavaca.org</a>         
       </p>
       
       <p>
         <strong>Desarrollado</strong>         
         con 
-        <a href="http://laravel.com">Laravel</a>,         
-        <a href="http://vuejs.org">Vue.js</a> y         
-        <a href="http://bulma.io">Bulma</a>        
+        <a target="_blank" href="http://laravel.com">Laravel</a>,         
+        <a target="_blank" href="http://vuejs.org">Vue.js</a> y         
+        <a target="_blank" href="http://bulma.io">Bulma</a>        
+      </p>
+
+      <p>
+        <a target="_blank" href="https://github.com/nan-apps/open_sheet_graphs">
+          <span class="icon" ><i class="fa fa-github"></i></span> Código
+        </a>
       </p>
       
     </div>
